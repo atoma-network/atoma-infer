332cd3ca498d3cd58be0abbe1ce46532c6f3449e
jorgeantonio21 <matroid@outlook.com>
2024-06-27 10:53:22 +0100

    add changes

--- src/paged_attention/mod.rs
+++ src/paged_attention/mod.rs
@@ -363,14 +363,14 @@ fn copy_blocks_t<T: CudaDType + DeviceRepr>(
     // Get CUDA slices for all tensors
     let key_caches_slices = key_caches
         .iter()
-        .map(|(storage, layout)| match storage {
+        .map(|(storage, layout)| match &*storage {
             Storage::Cuda(storage) => storage.as_cuda_slice::<T>().map(|s| (s, layout)),
             _ => candle_core::bail!("Only CUDA storage is supported"),
         })
         .collect::<Result<Vec<_>, _>>()?;
     let value_caches_slices = value_caches
         .iter()
-        .map(|(storage, layout)| match storage {
+        .map(|(storage, layout)| match &*storage {
             Storage::Cuda(storage) => storage.as_cuda_slice::<T>().map(|s| (s, layout)),
             _ => candle_core::bail!("Only CUDA storage is supported"),
         })
