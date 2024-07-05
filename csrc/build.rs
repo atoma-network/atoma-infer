1591f6bea467b9048ebbe04ecb1f65bac8335d57
jorgeantonio21 <matroid@outlook.com>
2024-07-05 15:58:20 +0100

    split kernels

--- csrc/build.rs
+++ csrc/build.rs
@@ -1,54 +1,68 @@
-use anyhow::Result;
-use std::fs::OpenOptions;
-use std::io::Write;
+// Build script to run nvcc and generate the C glue code for launching the flash-attention kernel.
+// The cuda build time is very long so one can set the CANDLE_FLASH_ATTN_BUILD_DIR environment
+// variable in order to cache the compiled artifacts and avoid recompiling too often.
+use anyhow::{Context, Result};
+use std::path::PathBuf;
 
-fn read_lines(filename: &str) -> Vec<String> {
-    let mut result = Vec::new();
-
-    for line in std::fs::read_to_string(filename).unwrap().lines() {
-        result.push(line.to_string())
-    }
-
-    result
-}
+const KERNEL_FILES: [&str; 2] = [
+    "kernels/attention/attention_kernels.cu",
+    "kernels/cache_kernels.cu",
+];
 
 fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/attention_kernels.cu");
-    println!("cargo:rerun-if-changed=src/cache_kernels.cu");
-
-    let builder = bindgen_cuda::Builder::default();
-    println!("cargo:info={builder:?}");
-    // builder.build_lib("libpagedattention.a");
-
-    let bindings = builder.build_ptx().unwrap();
-    bindings.write("src/lib.rs").unwrap();
-
-    let kernel_dir = std::path::PathBuf::from(".");
-    let absolute_kernel_dir = std::fs::canonicalize(&kernel_dir).unwrap();
-
-    println!(
-        "cargo:rustc-link-search=native={}",
-        absolute_kernel_dir.display()
-    );
-    println!("cargo:rustc-link-lib=pagedattention");
-
-    let contents = read_lines("src/lib.rs");
-    for line in contents {
-        if line == "pub mod ffi;" {
-            return Ok(());
+    for kernel_file in KERNEL_FILES.iter() {
+        println!("cargo:rerun-if-changed={kernel_file}");
+    }
+    println!("cargo:rerun-if-changed=kernels/flash_fwd_kernel.h");
+    println!("cargo:rerun-if-changed=kernels/flash_fwd_launch_template.h");
+    println!("cargo:rerun-if-changed=kernels/flash.h");
+    println!("cargo:rerun-if-changed=kernels/philox.cuh");
+    println!("cargo:rerun-if-changed=kernels/softmax.h");
+    println!("cargo:rerun-if-changed=kernels/utils.h");
+    println!("cargo:rerun-if-changed=kernels/kernel_traits.h");
+    println!("cargo:rerun-if-changed=kernels/block_info.h");
+    println!("cargo:rerun-if-changed=kernels/static_switch.h");
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").context("OUT_DIR not set")?);
+    let build_dir = match std::env::var("CANDLE_FLASH_ATTN_BUILD_DIR") {
+        Err(_) =>
+        {
+            #[allow(clippy::redundant_clone)]
+            out_dir.clone()
         }
-    }
+        Ok(build_dir) => {
+            let path = PathBuf::from(build_dir);
+            path.canonicalize().expect(&format!(
+                "Directory doesn't exists: {} (the current directory is {})",
+                &path.display(),
+                std::env::current_dir()?.display()
+            ))
+        }
+    };
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open("src/lib.rs")
-        .unwrap();
-    //Expose paged attention interface to Rust
-    if let Err(e) = writeln!(file, "pub mod ffi;") {
-        anyhow::bail!("error while building dependencies: {:?}\n", e,)
-    } else {
-        Ok(())
-    }
+    let kernels = KERNEL_FILES.iter().collect();
+    let builder = bindgen_cuda::Builder::default()
+        .kernel_paths(kernels)
+        .out_dir(build_dir.clone())
+        .arg("-std=c++17")
+        .arg("-O3")
+        .arg("-U__CUDA_NO_HALF_OPERATORS__")
+        .arg("-U__CUDA_NO_HALF_CONVERSIONS__")
+        .arg("-U__CUDA_NO_HALF2_OPERATORS__")
+        .arg("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
+        .arg("-Icutlass/include")
+        .arg("--expt-relaxed-constexpr")
+        .arg("--expt-extended-lambda")
+        .arg("--use_fast_math")
+        .arg("--verbose");
+
+    let out_file = build_dir.join("libflashattention.a");
+    builder.build_lib(out_file);
+
+    println!("cargo:rustc-link-search={}", build_dir.display());
+    println!("cargo:rustc-link-lib=flashattention");
+    println!("cargo:rustc-link-lib=dylib=cudart");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    Ok(())
 }
