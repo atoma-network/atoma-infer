157331bd1682f0fd726bb090d85607357a0b8e58
jorgeantonio21 <matroid@outlook.com>
2024-07-11 18:40:39 +0100

    add changes into build

--- csrc/build.rs
+++ csrc/build.rs
@@ -85,6 +85,9 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=kernels/kernel_traits.h");
     println!("cargo:rerun-if-changed=kernels/block_info.h");
     println!("cargo:rerun-if-changed=kernels/static_switch.h");
+    println!("cargo:rerun-if-changed=kernels/rotary.h");
+    println!("cargo:rerun-if-changed=kernels/alibi.h");
+    println!("cargo:rerun-if-changed=kernels/flash_api.cpp");
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").context("OUT_DIR not set")?);
     let build_dir = match std::env::var("ATOMA_FLASH_ATTN_BUILD_DIR") {
         Err(_) =>
@@ -106,17 +109,9 @@ fn main() -> Result<()> {
     let builder = bindgen_cuda::Builder::default()
         .kernel_paths(kernels)
         .out_dir(build_dir.clone())
-        .arg("-std=c++17")
-        .arg("-O3")
-        .arg("-U__CUDA_NO_HALF_OPERATORS__")
-        .arg("-U__CUDA_NO_HALF_CONVERSIONS__")
-        .arg("-U__CUDA_NO_HALF2_OPERATORS__")
-        .arg("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
-        .arg("-Icutlass/include")
-        .arg("--expt-relaxed-constexpr")
-        .arg("--expt-extended-lambda")
-        .arg("--use_fast_math")
-        .arg("--verbose");
+        .arg("-I./kernels");
+
+    println!("cargo:info={builder:?}");
 
     let out_file = build_dir.join("libflashattention.a");
     builder.build_lib(out_file);
