74f61fa3513b151c4432da857804377f243305cf
jorgeantonio21 <matroid@outlook.com>
2024-07-12 12:22:12 +0100

    add fixed number of rayon threads

--- csrc/build.rs
+++ csrc/build.rs
@@ -9,9 +9,9 @@ const KERNEL_FILES: [&str; 64] = [
     "kernels/flash_fwd_hdim32_bf16_sm80.cu",
     "kernels/flash_fwd_hdim32_fp16_causal_sm80.cu",
     "kernels/flash_fwd_hdim32_fp16_sm80.cu",
-    "kernels/flash_fwd_hdim64_bf16_causal_sm80.cu", 
-    "kernels/flash_fwd_hdim64_bf16_sm80.cu", 
-    "kernels/flash_fwd_hdim64_fp16_causal_sm80.cu", 
+    "kernels/flash_fwd_hdim64_bf16_causal_sm80.cu",
+    "kernels/flash_fwd_hdim64_bf16_sm80.cu",
+    "kernels/flash_fwd_hdim64_fp16_causal_sm80.cu",
     "kernels/flash_fwd_hdim64_fp16_sm80.cu",
     "kernels/flash_fwd_hdim96_bf16_causal_sm80.cu",
     "kernels/flash_fwd_hdim96_bf16_sm80.cu",
@@ -72,6 +72,7 @@ const KERNEL_FILES: [&str; 64] = [
 ];
 
 fn main() -> Result<()> {
+    std::env::set_var("RAYON_NUM_THREADS", "4");
     println!("cargo:rerun-if-changed=build.rs");
     for kernel_file in KERNEL_FILES.iter() {
         println!("cargo:rerun-if-changed={kernel_file}");
@@ -105,7 +106,11 @@ fn main() -> Result<()> {
         }
     };
     println!("cargo:warning={:?}", build_dir.display());
-    println!("cargo:warning={:?}", std::env::current_dir()?.display());
+
+    let current_dir = std::env::current_dir()?;
+    let cutlass_include_dir = current_dir.join("/cutlass/include");
+    let cutlass_include_arg = format!("-I{}", cutlass_include_dir.display());
+    let cutlass_include_arg = Box::leak(cutlass_include_arg.into_boxed_str());
 
     let kernels = KERNEL_FILES.iter().collect();
     let builder = bindgen_cuda::Builder::default()
@@ -117,7 +122,7 @@ fn main() -> Result<()> {
         .arg("-U__CUDA_NO_HALF_CONVERSIONS__")
         .arg("-U__CUDA_NO_HALF2_OPERATORS__")
         .arg("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
-        .arg("-Icsrc/cutlass/include")
+        .arg(cutlass_include_arg)
         .arg("--expt-relaxed-constexpr")
         .arg("--expt-extended-lambda")
         .arg("--use_fast_math")
@@ -125,12 +130,11 @@ fn main() -> Result<()> {
 
     println!("cargo:info={builder:?}");
 
-    let out_file = build_dir.join("libflashattention.a");
-    builder.build_lib(out_file);
+    let out_file = build_dir.join(format!("libflashattention.a"));
+    builder.build_lib(&out_file);
 
     println!("cargo:rustc-link-search={}", build_dir.display());
     println!("cargo:rustc-link-lib=flashattention");
-    println!("cargo:rustc-link-lib=flashattentionsplit");
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
