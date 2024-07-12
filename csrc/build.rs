45a5eac11d8eef6789bd6769ba21556f0201b4a3
jorgeantonio21 <matroid@outlook.com>
2024-07-12 11:35:32 +0100

    split kernels to reduce memory overhead for compilation

--- csrc/build.rs
+++ csrc/build.rs
@@ -9,9 +9,9 @@ const KERNEL_FILES: [&str; 64] = [
     "kernels/flash_fwd_hdim32_bf16_sm80.cu",
     "kernels/flash_fwd_hdim32_fp16_causal_sm80.cu",
     "kernels/flash_fwd_hdim32_fp16_sm80.cu",
-    "kernels/flash_fwd_hdim64_bf16_causal_sm80.cu",
-    "kernels/flash_fwd_hdim64_bf16_sm80.cu",
-    "kernels/flash_fwd_hdim64_fp16_causal_sm80.cu",
+    "kernels/flash_fwd_hdim64_bf16_causal_sm80.cu", 
+    "kernels/flash_fwd_hdim64_bf16_sm80.cu", 
+    "kernels/flash_fwd_hdim64_fp16_causal_sm80.cu", 
     "kernels/flash_fwd_hdim64_fp16_sm80.cu",
     "kernels/flash_fwd_hdim96_bf16_causal_sm80.cu",
     "kernels/flash_fwd_hdim96_bf16_sm80.cu",
@@ -72,7 +72,6 @@ const KERNEL_FILES: [&str; 64] = [
 ];
 
 fn main() -> Result<()> {
-    const NUM_RUNS: usize = 4;
     println!("cargo:rerun-if-changed=build.rs");
     for kernel_file in KERNEL_FILES.iter() {
         println!("cargo:rerun-if-changed={kernel_file}");
@@ -108,34 +107,30 @@ fn main() -> Result<()> {
     println!("cargo:warning={:?}", build_dir.display());
     println!("cargo:warning={:?}", std::env::current_dir()?.display());
 
-    let num_files = 64 / NUM_RUNS;
-    for i in 0..NUM_RUNS {
-        println!("cargo:warning={i}");
+    let kernels = KERNEL_FILES.iter().collect();
+    let builder = bindgen_cuda::Builder::default()
+        .kernel_paths(kernels)
+        .out_dir(build_dir.clone())
+        .arg("-std=c++17")
+        .arg("-O3")
+        .arg("-U__CUDA_NO_HALF_OPERATORS__")
+        .arg("-U__CUDA_NO_HALF_CONVERSIONS__")
+        .arg("-U__CUDA_NO_HALF2_OPERATORS__")
+        .arg("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
+        .arg("-Icsrc/cutlass/include")
+        .arg("--expt-relaxed-constexpr")
+        .arg("--expt-extended-lambda")
+        .arg("--use_fast_math")
+        .arg("--verbose");
 
-        let kernels = KERNEL_FILES[i * num_files..(i + 1) * num_files].iter().collect();
-        let builder = bindgen_cuda::Builder::default()
-            .kernel_paths(kernels)
-            .out_dir(build_dir.clone())
-            .arg("-std=c++17")
-            .arg("-O3")
-            .arg("-U__CUDA_NO_HALF_OPERATORS__")
-            .arg("-U__CUDA_NO_HALF_CONVERSIONS__")
-            .arg("-U__CUDA_NO_HALF2_OPERATORS__")
-            .arg("-U__CUDA_NO_BFLOAT16_CONVERSIONS__")
-            .arg("-Icsrc/cutlass/include")
-            .arg("--expt-relaxed-constexpr")
-            .arg("--expt-extended-lambda")
-            .arg("--use_fast_math");
+    println!("cargo:info={builder:?}");
 
-        println!("cargo:info={builder:?}");
-
-        let out_file = build_dir.join(format!("libflashattention_{i}.a")); 
-        builder.build_lib(out_file);
-
-        println!("cargo:rustc-link-lib=flashattention_{i}");
-    }
+    let out_file = build_dir.join("libflashattention.a");
+    builder.build_lib(out_file);
 
     println!("cargo:rustc-link-search={}", build_dir.display());
+    println!("cargo:rustc-link-lib=flashattention");
+    println!("cargo:rustc-link-lib=flashattentionsplit");
     println!("cargo:rustc-link-lib=dylib=cudart");
     println!("cargo:rustc-link-lib=dylib=stdc++");
 
