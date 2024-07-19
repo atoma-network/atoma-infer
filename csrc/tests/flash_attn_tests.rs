3736f155aa698853d852e04a5cf7181877b2ec9f
jorgeantonio21 <matroid@outlook.com>
2024-07-19 11:37:39 +0100

    refactor test

--- csrc/tests/flash_attn_tests.rs
+++ csrc/tests/flash_attn_tests.rs
@@ -148,9 +148,9 @@ fn flash_attn_varlen_with_block_table() -> Result<()> {
     let seqlens_k = Tensor::new(&[0u32, 16u32, 32u32], &device)?;
 
     let ys = {
-        // let q = q.transpose(0, 1)?;
-        // let k = k.transpose(0, 1)?;
-        // let v = v.transpose(0, 1)?;
+        let q = q.transpose(1, 2)?.transpose(0, 1)?;
+        let k = k.transpose(1, 2)?.transpose(0, 1)?;
+        let v = v.transpose(1, 2)?.transpose(0, 1)?;
         let block_table = Some(Tensor::arange(0u32, 4, &device)?.reshape((2, 2))?);
         csrc::flash_attn_varlen_with_block_table(
             &q,
@@ -166,7 +166,8 @@ fn flash_attn_varlen_with_block_table() -> Result<()> {
             None,
             block_table,
         )?
-        // .transpose(0, 1)?
+        .transpose(1, 2)?
+        .transpose(0, 1)?
     };
     let ys = ys.to_dtype(DType::F32)?;
 
