9a9e0aa3cb6a65d57715c19ee7af7376dbe40442
jorgeantonio21 <matroid@outlook.com>
2024-07-23 09:21:17 +0100

    add changes

--- csrc/tests/cache_manager_tests.rs
+++ csrc/tests/cache_manager_tests.rs
@@ -264,11 +264,11 @@ mod copy_blocks {
 
         panic!(
             "vec1 = {:?} \n vec2 = {:?}",
-            key_caches_refs[0].flatten_all().unwrap().to_vec1().unwrap(),
+            key_caches_refs[0].flatten_all().unwrap().to_vec1::<half::f16>().unwrap(),
             original_key_caches[0]
                 .flatten_all()
                 .unwrap()
-                .to_vec1()
+                .to_vec1::<half::f16>()
                 .unwrap()
         );
 
