8f8778aece2c2375ebf8bb64fa5cff8b10230f55
jorgeantonio21 <matroid@outlook.com>
2024-07-25 11:55:00 +0100

    add changes

--- src/flash_attention.rs
+++ src/flash_attention.rs
@@ -407,7 +407,7 @@ impl FlashAttention {
                 decoding_metadata.sequence_lengths.as_ref(),
                 None,
             )?;
-            // output.slice_assign(&[num_prefill_tokens.., 0.., 0..], &out)?;
+            output.slice_assign(&[num_prefill_tokens.., 0.., 0..], &out.squeeze(1)?)?;
         }
 
         Ok(output)
