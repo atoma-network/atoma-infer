ce3cd49d1b180addacb3df130e8d546a944e6bc6
jorgeantonio21 <matroid@outlook.com>
2024-07-25 11:55:20 +0100

    add changes

--- src/flash_attention.rs
+++ src/flash_attention.rs
@@ -558,7 +558,7 @@ mod tests {
 
         let result = flash_attention.forward(&q, &k, &v, &kv_cache, attention_metadata);
 
-        assert!(result.is_ok());
+        // assert!(result.is_ok());
         let output = result.unwrap();
         // assert_eq!(output.shape().dims(), &[15, 512]);
         // assert!(!output.eq(0.).unwrap().flatten_all().unwrap().to_vec1().unwrap().iter().any(|&x| x == 0.));
