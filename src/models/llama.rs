e40b15033246f8e977402167f2aa120c2794ea8c
jorgeantonio21 <matroid@outlook.com>
2024-07-26 21:33:52 +0100

    changes

--- src/models/llama.rs
+++ src/models/llama.rs
@@ -16,7 +16,7 @@ pub fn save_tensor_to_file(tensor: &Tensor, filename: &str) -> Result<()> {
             .to_vec1::<f64>()?,
     )
     .unwrap();
-    let mut file = File::create(PathBuf::from(filename))?;
+    let mut file = std::fs::File::create(std::path::PathBuf::from(filename))?;
     file.write_all(json_output.as_bytes())?;
     Ok(())
 }
@@ -423,7 +423,7 @@ impl Llama {
         for (i, block) in self.blocks.iter_mut().enumerate() {
             x = block.forward(&x, input_positions, &kv_caches[i], &attention_metadata)?;
         }
-       save_tensor_to_file(&x, "attn_output")?;
+        save_tensor_to_file(&x, "attn_output")?;
         let x = self.ln_f.forward(&x)?;
         let x = x.index_select(selected_token_indices, 1)?.contiguous()?;
         let logits = self.lm_head.forward(&x)?;
