fde82cd84d2c8aff3e8647dd2a67f20b6ebbeac9
jorgeantonio21 <matroid@outlook.com>
2024-07-29 13:17:16 +0100

    add changes

--- src/flash_attention.rs
+++ src/flash_attention.rs
@@ -581,7 +581,7 @@ mod tests {
     }
 
     #[test]
-    fn test_forwared_with_varlen() {
+    fn test_forward_with_varlen() {
         let device = Device::new_cuda(0).unwrap();
         let q = Tensor::arange(0u32, 48, &device)
             .unwrap()
