137e482d2b4f38791ac136a9682422b545f4fac0
jorgeantonio21 <matroid@outlook.com>
2024-08-01 21:29:58 +0100

    changes

--- src/models/llama.rs
+++ src/models/llama.rs
@@ -545,7 +545,7 @@ mod tests {
         let input = Tensor::new(&tokens[..], &device)?.unsqueeze(0)?;
         let attention_metadata = FlashAttentionMetadata {
             context_lengths: Some(Tensor::from_vec(vec![tokens.len() as u32], (1,), &device)?),
-            slot_mapping: Tensor::arange(0, tokens.len() as i64 - 1, &device)?,
+            slot_mapping: Tensor::new(&[tokens.len() as i64 - 1], &device)?,
             decoding_metadata: None,
             num_prefill_tokens: tokens.len(),
             num_decoding_tokens: 0,
