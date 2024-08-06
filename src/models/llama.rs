048c3bb5485a3a6f3cb8f5f09220e6d091b5bb85
jorgeantonio21 <matroid@outlook.com>
2024-08-06 16:40:06 +0100

    testing

--- src/models/llama.rs
+++ src/models/llama.rs
@@ -722,61 +722,61 @@ mod tests {
             (1,),
             &device,
         )?;
-        let input = Tensor::from_vec(tokens.clone().into_iter().flatten().collect(), (1,), &device)?;
-        let attention_metadata = FlashAttentionMetadata {
-            context_lengths: Some(Tensor::from_vec(
-                tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>(),
-                (1,),
-                &device,
-            )?),
-            slot_mapping: Tensor::from_vec(
-                tokens
-                    .iter()
-                    .enumerate()
-                    .flat_map(|(i, ts)| {
-                        ((i * token_size_allocation) as i64)
-                            ..((i * token_size_allocation + ts.len()) as i64)
-                    })
-                    .collect(),
-                (num_prefill_tokens,),
-                &device,
-            )?, // [0, .., num_tokens]
-            decoding_metadata: None,
-            num_prefill_tokens: tokens.iter().map(|ts| ts.len()).sum::<usize>(),
-            num_decoding_tokens: 0,
-            prefill_metadata: Some(FlashAttentionPrefillMetadata {
-                block_tables: None,
-                max_query_length: Some(max_tokens_len),
-                max_prefill_sequence_length: max_tokens_len,
-                query_start_locations: Some(Tensor::from_vec(
-                    vec![0u32]
-                        .into_iter()
-                        .chain(tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>())
-                        .collect(),
-                    (tokens.len() + 1,),
-                    &device,
-                )?),
-                sequence_start_locations: Some(Tensor::from_vec(
-                    vec![0u32]
-                        .into_iter()
-                        .chain(tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>())
-                        .collect(),
-                    (tokens.len() + 1,),
-                    &device,
-                )?),
-                sequence_lengths: Some(Tensor::from_vec(
-                    tokens.iter().map(|ts| ts.len() as u32).collect(),
-                    (tokens.len(),),
-                    &device,
-                )?),
-            }),
-        };
+        // let input = Tensor::from_vec(tokens.clone().into_iter().flatten().collect(), (1,), &device)?;
+        // let attention_metadata = FlashAttentionMetadata {
+        //     context_lengths: Some(Tensor::from_vec(
+        //         tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>(),
+        //         (1,),
+        //         &device,
+        //     )?),
+        //     slot_mapping: Tensor::from_vec(
+        //         tokens
+        //             .iter()
+        //             .enumerate()
+        //             .flat_map(|(i, ts)| {
+        //                 ((i * token_size_allocation) as i64)
+        //                     ..((i * token_size_allocation + ts.len()) as i64)
+        //             })
+        //             .collect(),
+        //         (num_prefill_tokens,),
+        //         &device,
+        //     )?, // [0, .., num_tokens]
+        //     decoding_metadata: None,
+        //     num_prefill_tokens: tokens.iter().map(|ts| ts.len()).sum::<usize>(),
+        //     num_decoding_tokens: 0,
+        //     prefill_metadata: Some(FlashAttentionPrefillMetadata {
+        //         block_tables: None,
+        //         max_query_length: Some(max_tokens_len),
+        //         max_prefill_sequence_length: max_tokens_len,
+        //         query_start_locations: Some(Tensor::from_vec(
+        //             vec![0u32]
+        //                 .into_iter()
+        //                 .chain(tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>())
+        //                 .collect(),
+        //             (tokens.len() + 1,),
+        //             &device,
+        //         )?),
+        //         sequence_start_locations: Some(Tensor::from_vec(
+        //             vec![0u32]
+        //                 .into_iter()
+        //                 .chain(tokens.iter().map(|ts| ts.len() as u32).collect::<Vec<_>>())
+        //                 .collect(),
+        //             (tokens.len() + 1,),
+        //             &device,
+        //         )?),
+        //         sequence_lengths: Some(Tensor::from_vec(
+        //             tokens.iter().map(|ts| ts.len() as u32).collect(),
+        //             (tokens.len(),),
+        //             &device,
+        //         )?),
+        //     }),
+        // };
 
-        let selected_token_indices = Tensor::from_vec(
-            tokens.iter().map(|ts| ts.len() as u32 - 1).collect(),
-            (tokens.len(),),
-            &device,
-        )?;
+        // let selected_token_indices = Tensor::from_vec(
+        //     tokens.iter().map(|ts| ts.len() as u32 - 1).collect(),
+        //     (tokens.len(),),
+        //     &device,
+        // )?;
         // let logits = llama_model.forward(
         //     &input,
         //     &input_positions,
