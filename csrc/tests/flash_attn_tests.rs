23600515926764c8119900eff42758623e658529
jorgeantonio21 <matroid@outlook.com>
2024-08-13 16:55:27 +0100

    adjust block table types in tests

--- csrc/tests/flash_attn_tests.rs
+++ csrc/tests/flash_attn_tests.rs
@@ -154,7 +154,7 @@ fn flash_attn_varlen_with_block_table() -> Result<()> {
     let seqlens_k = Tensor::new(&[0u32, 32u32, 64u32], &device)?;
 
     let ys = {
-        let block_table = Some(Tensor::arange(0i64, 4, &device)?.reshape((2, 2))?);
+        let block_table = Some(Tensor::arange(0u32, 4, &device)?.reshape((2, 2))?);
         csrc::flash_attn_varlen_with_block_table(
             &q,
             &k,
@@ -289,7 +289,7 @@ fn test_flash_attn_kv_cache_with_block_table() -> Result<()> {
     let seqlens_k = Tensor::from_vec((0u32..=32).collect::<Vec<_>>(), (33,), &device)?;
 
     let should_be_ys = {
-        let block_table = Some(Tensor::arange(0i64, 64, &device)?.reshape((32, 2))?);
+        let block_table = Some(Tensor::arange(0u32, 64, &device)?.reshape((32, 2))?);
         csrc::flash_attn_varlen_with_block_table(
             &q,
             &k,
