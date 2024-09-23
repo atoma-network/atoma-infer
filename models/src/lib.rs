be97203103678f9c036126303d6903b9ae054826
Fishon Amos <amosfishon@gmail.com>
2024-09-23 10:45:08 +0100

    merge

--- models/src/lib.rs
+++ models/src/lib.rs
@@ -1,10 +1,11 @@
 pub mod flash_attention;
 pub mod llama;
+pub mod mistral;
 
 pub use flash_attention::{
     FlashAttention, FlashAttentionDecodingMetadata, FlashAttentionMetadata,
     FlashAttentionPrefillMetadata,
 };
 pub use models::phi3::Phi3Model as Phi3;
-pub use models::{Llama, MistralModel};
 pub use llama::Llama;
+pub use mistral::MistralModel;
--- src/models/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod llama;
-pub mod phi3;
-pub mod mistral;
-
-pub use llama::Llama;
-pub use phi3::Phi3Model as Phi3;
-
-pub use mistral::MistralModel;
-
