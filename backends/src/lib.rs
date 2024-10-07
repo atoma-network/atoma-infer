8a4b299a360c1f9daa8c4802007b0c73dd9ced7c
Jorge António <matroid@outlook.com>
2024-10-07 14:42:38 +0100

    feat: update readme (#99)
    * first commit
    
    * add more info on readme
    
    * add more details
    
    * add more details
    
    * add more details
    
    * add more details
    
    * add changes
    
    * add changes
    
    * update readme
    
    * further updates
    
    * update logo
    
    * update logo
    
    * experiment logo size
    
    * experiment logo size
    
    * experiment logo size
    
    * experiment logo size
    
    * experiment logo size

--- backends/src/lib.rs
+++ backends/src/lib.rs
@@ -2,10 +2,15 @@
 pub use atoma_vllm_backend::{
     llm_engine::GenerateRequestOutput,
     llm_service::{LlmService, LlmServiceError},
-    models::llama::LlamaModel,
     types::{GenerateParameters, GenerateRequest},
     validation::Validation,
 };
+
+#[cfg(feature = "nccl")]
+pub use atoma_vllm_backend::models::llama_nccl::LlamaModel;
+
+#[cfg(not(feature = "nccl"))]
+pub use atoma_vllm_backend::models::llama::LlamaModel;
 
 #[cfg(not(feature = "vllm"))]
 compile_error!("vllm backend is not enabled. Please compile with --features vllm");
